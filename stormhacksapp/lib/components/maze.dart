d4edbab7a049375d732adfc59a7156cfc244104e
Diana Nguyen <diananguyen604@gmail.com>
2025-10-04 14:51:12 -0700

    added test file to import to main.dart

---
+++ stormhacksapp/lib/components/maze.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Maze extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello, this is a test!");
+  }
+}
