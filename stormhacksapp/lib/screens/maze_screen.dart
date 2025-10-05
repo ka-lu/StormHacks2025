6cab75f5b91441e09774dd14f6e4f2cb3315245d
Diana Nguyen <diananguyen604@gmail.com>
2025-10-05 02:34:35 -0700

    Tweaked maze_screen to improve experience

--- stormhacksapp/lib/screens/maze_screen.dart
+++ stormhacksapp/lib/screens/maze_screen.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 import 'dart:async';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:stormhacksapp/screens/home_screen.dart';
-import 'home_screen.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter/scheduler.dart';
 
 
 class Maze extends StatefulWidget {
@@ -33,9 +30,14 @@ class _mazeState extends State<Maze> {
   double circleY = 0;
 
   double gravity = 2.0;
-  double horizontalSpeed = 5.0;
+  double horizontalSpeed = 3.0;
 
   List<Rect> platforms = [];
+
+  StreamSubscription<AccelerometerEvent>? _accelerometerSubscription;
+  double _tiltSensitivity = 3.0;
+
+  Timer? _gravityTimer;
 
   @override
   void initState() {
@@ -48,12 +50,32 @@ class _mazeState extends State<Maze> {
       Rect.fromLTWH(70, 220, 60, 20),
     ];
 
-    Timer.periodic(const Duration(milliseconds: 256), (timer) {
-      setState(() {
-        applyGravity();
-        clampBoundaries();
-      });
+    _accelerometerSubscription = accelerometerEventStream().listen((AccelerometerEvent event) {
+      if (mounted) {
+        setState(() {
+          double newX = circleX + event.x * _tiltSensitivity;
+          if (!checkCollision(newX, circleY)) {
+            circleX += newX;
+          }
+        });
+      }
     });
+
+    _gravityTimer = Timer.periodic(const Duration(milliseconds: 256), (timer) {
+      if (mounted) {
+        setState(() {
+          applyGravity();
+          clampBoundaries();
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _gravityTimer?.cancel();
+    _accelerometerSubscription?.cancel();
+    super.dispose();
   }
 
   bool goalReached = false;
@@ -62,11 +84,13 @@ class _mazeState extends State<Maze> {
     if (!goalReached && (circleX > 125 && circleX < 165) && (circleY > 275)) {
       goalReached = true;
       _sendSMS();
+      _gravityTimer?.cancel();
+      _accelerometerSubscription?.cancel();
       Navigator.pushReplacement(
       context,
         MaterialPageRoute(
           builder: (context) => ChatScreen()
-          ), 
+        ), 
       );
     }
     if (circleY > maxY) {
@@ -91,18 +115,37 @@ class _mazeState extends State<Maze> {
       }
     }
     if (!onPlatform) {
-      circleY = nextY;
+      if (!checkCollision(circleX, nextY)) {
+        circleY = nextY;
+      }
     }
+  }
+
+  bool checkCollision(double newX, double newY) {
+    Rect ballRect = Rect.fromLTWH(newX, newY, 20, 20);
+
+    for (Rect platform in platforms) {
+      if (ballRect.overlaps(platform)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   KeyEventResult handleKey(FocusNode node, KeyEvent event) {
     if (event is KeyDownEvent || event is KeyRepeatEvent) {
       if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
-        circleX -= horizontalSpeed;
+        double newX = circleX - horizontalSpeed;
+        if (!checkCollision(newX, circleY)) {
+          circleX = newX;
+        }
         setState(() {});
       }
       else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
-        circleX += horizontalSpeed;
+        double newX = circleX + horizontalSpeed;
+        if (!checkCollision(newX, circleY)) {
+          circleX = newX;
+        }
         setState(() {});
       }
     }
@@ -112,60 +155,84 @@ class _mazeState extends State<Maze> {
   @override
   Widget build(BuildContext context) {
     return Focus(
-      autofocus: true, // <-- Ensures it receives keyboard events immediately
+      autofocus: true,
       onKeyEvent: handleKey,
       child: Scaffold(
         body: Center(
-          child: Stack(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Stack(
+                children: [
+                  
+                  Container(
+                    width: 300,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
 
-              Container(
-                width: 300,
-                height: 300,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(20),
-                ),
+                  ...platforms.map((p) => Positioned(
+                    left: p.left,
+                    top: p.top,
+                    child: Container(
+                      width: p.width,
+                      height: p.height,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 16, 70, 133),
+                      ),
+                    ),
+                  )),
+
+                  Positioned(
+                    left: circleX,
+                    top: circleY,
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+
+                  Positioned(
+                    left: 125,
+                    top: 275,
+                    child: Container(
+                      width: 40,
+                      height: 25,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
               ),
 
-              ...platforms.map((p) => Positioned(
-                left: p.left,
-                top: p.top,
-                child: Container(
-                  width: p.width,
-                  height: p.height,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 16, 70, 133),
-                    //borderRadius: BorderRadius.circular(10),
-                  ),
+              SizedBox(height: 10),
+              Text(
+                'Tilt your phone to move the ball!',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-              )),
-
-              Positioned(
-                left: circleX,
-                top: circleY,
-                child: Container(
-                  width: 20,
-                  height: 20,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 4),
+              Text(
+                'Reach the black hole to send',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
                 ),
+                textAlign: TextAlign.center,
               ),
 
-              Positioned(
-                left: 125,
-                top: 275,
-                child: Container(
-                  width: 40,
-                  height: 25,
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 0, 0, 0),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
             ],
           ),
         ),
